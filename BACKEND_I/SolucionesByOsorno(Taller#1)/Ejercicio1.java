0391fd702a4996fd3adacc4f1b5fb18e1388d6c7
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-02-27 22:31:03 -0500

    Cambio

--- BACKEND_I/SolucionesByOsorno(Taller#1)/Ejercicio1.java
+++ BACKEND_I/SolucionesByOsorno(Taller#1)/Ejercicio1.java
@@ -1,5 +1,7 @@
 public class Ejercicio1{
     //FizzBuzz
+    //El truco de este ejercicio es aprovechar el operador modulo %.
+    //(*)Observacion: n%m == 0 si n es divisible por m.
     public static void main(String[] args){
         for (int i = 1; i <= 100; i++) {
             if(i%3==0 && i%5==0){
