3d216193bf4b71688e9a8ef76a201304cbb98f01
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-03-01 11:32:40 -0500

    Actualizando

--- BACKEND_I/Talleres/SolucionesByOsorno(Taller#1)/Ejercicio2.java
+++ BACKEND_I/Talleres/SolucionesByOsorno(Taller#1)/Ejercicio2.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
 import java.util.Random;
 public class Ejercicio2 {
     //Primera idea: Vamos a crear una funcion que tome la palabra ingresada
     // y este haga permutaciones con sus letras. Luego veremos que mas hacer para arrojar si las palabras
     // son o no anagramas
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese una palabra: ");
-        String texto = sc.next();
+        String texto = "amor";
         System.out.println(permutacion(texto));
     }
     //Hasta el momento el unico problema es que quizas no tome nunca la posicion de una letra
-    //problemas de aleoriedad
+    //problemas de aleoriedad (HAY QUE REGAÑAR OBLIGAR AL NIÑO)
     public static char[] permutacion(String palabra){
         Random rd = new Random();
-        char[] V = new char[palabra.length()]; //El tamaño es fijo (Esto puede ser una limitacion)
+        int tamaño = palabra.length();
+        char[] V = new char[tamaño];
         for (int i = 0; i < V.length+1; i++) {
             int Num = rd.nextInt(100);
-            char L = palabra.charAt(Num%palabra.length());
+            char L = palabra.charAt(Num%tamaño);
             if(!contieneL(V,L,i)){
                 V[i] = L;
             }  
@@ -32,6 +30,11 @@ public class Ejercicio2 {
                 return true; 
             }
         }
+        return false;
+    }
+    //Crearemos otra funcion que compare dos vectores y mire si a uno le falta una o no otra letra
+    public static boolean comparacion(char[] A, char[] B){
+        
         return false;
     }
 }
