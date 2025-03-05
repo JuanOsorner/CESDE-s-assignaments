0b64401e04f62e89db9607967d890730a4367a10
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-03-04 22:28:54 -0500

    Avanzando

--- BACKEND_I/Talleres/SolucionesByOsorno(Taller#1)/Ejercicio2.java
+++ BACKEND_I/Talleres/SolucionesByOsorno(Taller#1)/Ejercicio2.java
@@ -5,13 +5,7 @@ public class Ejercicio2 {
     // son o no anagramas
     public static void main(String[] args){
         String texto = "amor";
-        char[] W = new char[4];
-        //Mientras tanto creare este ciclo para llenar el vector W con la palabra amor
-        for (int i = 0; i < W.length; i++) {
-            W[i] = texto.charAt(i);
-        }
         System.out.println(permutacion(texto));
-        System.out.println(W);
     }
     //Hasta el momento el unico problema es que quizas no tome nunca la posicion de una letra
     //problemas de aleoriedad (HAY QUE REGAÑAR OBLIGAR AL NIÑO)
@@ -19,7 +13,7 @@ public class Ejercicio2 {
         Random rd = new Random();
         int tamaño = palabra.length();
         char[] V = new char[tamaño];
-        for (int i = 0; i < V.length+1; i++) {
+        for (int i = 0; i < V.length; i++) {
             int Num = rd.nextInt(100);
             char L = palabra.charAt(Num%tamaño);
             if(!contieneL(V,L,i)){
@@ -36,11 +30,6 @@ public class Ejercicio2 {
                 return true; 
             }
         }
-        return false;
-    }
-    //Crearemos otra funcion que compare dos vectores y mire si a uno le falta una o no otra letra
-    //Esta funcion compara si le faltan
-    public static boolean comparacion(char[] A, char[] B){
         return false;
     }
 }
---
+++ BACKEND_I/Talleres/SolucionesByOsorno(Taller#1)/test.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+public class test {
+    public static void main(String[] args){
+        //Crearemos una funcion que tome una palabra y la permute
+        String palabra = "amor";
+        System.out.println(permutaciones(palabra));
+    }
+    public static char[] permutaciones(String S){
+        Random rd = new Random();
+        int tamaño = S.length();
+        char b = '.';
+        char [] V = new char[tamaño];
+        for (int j = 0; j < V.length; j++) {
+            int enteroA = rd.nextInt(); 
+            char a = S.charAt(enteroA%tamaño);
+            if(b == a){
+                a = S.charAt((enteroA+1)%tamaño);
+                V[j] = a;
+            } else {
+                V[j] = a;
+            }
+            b = a;
+        }
+        return V;
+    }
+    //Tomemos la letra a y la palabra amor, si a esta en amor, elegir otra letra distinta. 
+}
