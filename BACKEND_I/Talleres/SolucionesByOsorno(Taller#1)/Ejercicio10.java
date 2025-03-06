d25df237216624e99b256f8d1440694eb58aa923
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-03-06 16:22:52 -0500

    Ejercicio 10

---
+++ BACKEND_I/Talleres/SolucionesByOsorno(Taller#1)/Ejercicio10.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ejercicio10 {
+    //PARA ESTE ALGORITMO VAMOS A HACER USO DE LA FORMULA DE ZELLER (INVESTIGAR)
+    //Consideremos int piso = (int) Math.floor(numero)
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el año: ");
+        int año = sc.nextInt();
+        System.out.println("Ajuste \nEnero = 13 \nFebrero = 14");
+        System.out.println("Ingrese el mes: ");
+        int mes = sc.nextInt();
+        System.out.println(sino(año, mes));
+    }
+    //x == año, y == mes
+    public static boolean sino(int x, int y){
+        int K = x%100;
+        int J = (int) Math.floor(x/100);
+        int A = (int) Math.floor((13*(y+1))/5);
+        int B = (int) Math.floor(K/4);
+        int C = (int) Math.floor(J/4);
+        for (int i = 0; i < 7; i++) {
+            int q = (i - (A + K + B + C + 5*J))%7;
+            if(q == 13){
+                return true;
+            }
+        }
+        return false;
+    }
+}
